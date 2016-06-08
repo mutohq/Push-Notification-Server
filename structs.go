2dda3e1d0877a1a121a6f0dc22036a3a850da4d2
sbnsl-srm <sbnsl.srm@gmail.com>
2016-06-08 15:21:40 +0530

    All the struct fields and their JSON tags.
    The file contains all the structs used and their JSON corresponding tags as allocated by GCM and APNS.

--- structs.go
+++ structs.go
@@ -3,23 +3,19 @@ package muto_gcm
 const GcmAPI = "https://android.googleapis.com/gcm/send"
 const Authorization = "key=AIzaSyBfI5t4-GW5VovfzQ6BpvhTd2dkUB7L9R0"
 
-//const ApnsAPI = "api.development.push.apple.com:443"
-// const Cert = "/Users/shivam_mac/Downloads/cert.pem"
-
 const ApnsAPI = "gateway.sandbox.push.apple.com:2195"
 
-// const Key = "/Users/shivam_mac/Downloads/key.pem"
 
+// Stores DevviceID/Token and Platform for any given device
 type Device struct {
 	DeviceID string `json:"deviceid,omitempty"`
 	Platform string `json:"platform,omitempty"`
 }
 
-// type Devices struct {
-// 	DevicesList []Device `json"deviceslist,omitempty"`
-// }
-
+// A Local struct to integrate all the supported fields of both APIs before bifurcating the payload based on Platform.
 type Combined struct {
+	
+	// GCM fields
 	TargetDeviceIDs       []string    `json:"registration_ids,omitempty"`
 	CollapseKey           string      `json:"collapse_key,omitempty"`
 	Priority              string      `json:"priority,omitempty"`
@@ -30,15 +26,17 @@ type Combined struct {
 	DryRun                bool        `json:"dry_run,omitempty"`
 	Payload               PayloadBody `json:"notification,omitempty"` ////  notification
 	Contents              Content     `json:"contents,omitempty"`
-
+	
+	 // APNS fields
 	Alert            interface{} `json:"alert,omitempty"` ////    alert
 	Badge            int         `json:"badge,omitempty"`
 	Sound            string      `json:"sound,omitempty"`
 	ContentAvailable int         `json:"content-available,omitempty"`
 	Category         string      `json:"category,omitempty"`
-
+	
+	// An array to store the DeviceIDs and their Platforms
 	DevicesList []Device `json"deviceslist,omitempty"`
-
+	
 	// Not a standard field. Included to incorporate AlertDictionary type in Combined type.
 	AlertDict AlertDictionary `json:"alertdic,omitempty"`
 }
@@ -56,6 +54,7 @@ type Notification struct {
 	DryRun                bool        `json:"dry_run,omitempty"`
 }
 
+// GCM Payload fields
 type PayloadBody struct {
 	Title        string   `json:"title,omitempty"`
 	Body         string   `json:"body,omitempty"`
@@ -70,6 +69,7 @@ type PayloadBody struct {
 	TitleLocArgs []string `json:"title_loc_args,omitempty"`
 }
 
+// APNS original struct format
 type AlertDictionary struct {
 	Title string `json:"title,omitempty"`
 	Body  string `json:"body,omitempty"`
@@ -82,11 +82,13 @@ type AlertDictionary struct {
 	LaunchImage  string   `json:"launch-image,omitempty"`
 }
 
+// Captures the common features of GCM and APNS payload
 type Content struct {
 	Title string `json:"title,omitempty"`
 	Body  string `json:"body,omitempty"`
 }
 
+// Reuired to query the Database and find out DeviceID and Platform.
 type Request struct {
 	Username string `json:"username,omitempty"`
 	Email    string `json:"email,omitempty"`
